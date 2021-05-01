2fbef4c414a80cb948f626c13aacd3cf55d495b3
EnricoFiasche <enrico.fiasche@gmail.com>
2021-05-01 15:27:30 +0000

    Working action, missing cancelGoal

--- scripts/user_interface.py
+++ scripts/user_interface.py
@@ -3,20 +3,22 @@ import time
 from rt2_assignment1.srv import Command
 
 def main():
-    rospy.init_node('user_interface')
-    ui_client = rospy.ServiceProxy('/user_interface', Command)
-    time.sleep(10)
-    rate = rospy.Rate(20)
-    x = int(input("\nPress 1 to start the robot "))
-    rospy.loginfo("PULSANTE PREMUTO")
-    while not rospy.is_shutdown():
-        if (x == 1):
-            ui_client("start")
-            x = int(input("\nPress 0 to stop the robot "))
-        else:
-            print("Please wait, the robot is going to stop when the position will be reached")
-            ui_client("stop")
-            x = int(input("\nPress 1 to start the robot "))
-            
+	rospy.init_node('user_interface')
+	ui_client = rospy.ServiceProxy('/user_interface', Command)
+	time.sleep(10)
+	rate = rospy.Rate(20)
+	x = int(input("\nPress 1 to start the robot "))
+	while not rospy.is_shutdown():
+		if (x == 1):
+			ui_client("start")
+			x = int(input("\nPress 0 to stop the robot "))
+		elif (x == 0):
+			print("Please wait, the robot is going to stop when the position will be reached")
+			ui_client("stop")
+			x = int(input("\nPress 1 to start the robot "))
+		else:
+			x = int(input("\nCommand not recognized, try again "))
+			
+			
 if __name__ == '__main__':
     main()
--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -14,56 +14,59 @@ bool user_interface(rt2_assignment1::Command::Request &req, rt2_assignment1::Com
     else {
     	start = false;
     }
+    
+    std::cout << "Changing START value: " << start << std::endl;
     return true;
 }
 
 int main(int argc, char **argv)
 {
-   ros::init(argc, argv, "state_machine");
-   ros::NodeHandle n;
-   ros::ServiceServer service= n.advertiseService("/user_interface", user_interface);
-   ros::ServiceClient client_rp = n.serviceClient<rt2_assignment1::RandomPosition>("/position_server");
+	ros::init(argc, argv, "state_machine");
+	ros::NodeHandle n;
+	ros::ServiceServer service= n.advertiseService("/user_interface", user_interface);
+	ros::ServiceClient client_rp = n.serviceClient<rt2_assignment1::RandomPosition>("/position_server");
    
-   actionlib::SimpleActionClient<rt2_assignment1::PositionAction> action_client("go_to_point", true);
+	actionlib::SimpleActionClient<rt2_assignment1::PositionAction> action_client("go_to_point", true);
 
-   //std::cout << "WAIT FOR ACTION SERVER" << std::endl;
+	//std::cout << "WAIT FOR ACTION SERVER" << std::endl;
 	while(!action_client.waitForServer(ros::Duration(5.0))){
 		ROS_INFO("Waiting for the move_base action server to come up");
 	}
+	   
+	rt2_assignment1::RandomPosition rp;
+	rp.request.x_max = 5.0;
+	rp.request.x_min = -5.0;
+	rp.request.y_max = 5.0;
+	rp.request.y_min = -5.0;
    
-   //std::cout << "ACTION SERVER IS OKAY!" << std::endl;
-   ROS_INFO("ACTION SERVER IS OKAY");
-   
-   rt2_assignment1::RandomPosition rp;
-   rp.request.x_max = 5.0;
-   rp.request.x_min = -5.0;
-   rp.request.y_max = 5.0;
-   rp.request.y_min = -5.0;
-   
-   ROS_INFO("EVERYTHING OKAY!");
-   //std::cout << "EVERYTHING OKAY!" << std::endl;
-   
-   while(ros::ok()){
-   	
-	ros::spinOnce();
-   	if (start){
-   		client_rp.call(rp);
-   		
-   		rt2_assignment1::PositionGoal goal;
-   		goal.x = rp.response.x; 
-   		goal.y = rp.response.y;
-   		goal.theta = rp.response.theta;
-   		
-   		action_client.sendGoal(goal);
-   		
-   		std::cout << "\nGoing to the position: x= " << goal.x << " y= " << goal.y << " theta = " << goal.theta << std::endl;
+	while(ros::ok()){
+		ros::spinOnce();
 		
-		action_client.waitForResult(ros::Duration(30.0));
+		if (start){
+			client_rp.call(rp);
+   		
+			rt2_assignment1::PositionGoal goal;
+			goal.x = rp.response.x; 
+			goal.y = rp.response.y;
+			goal.theta = rp.response.theta;
+   		
+			action_client.sendGoal(goal);
+   		
+			std::cout << "\nGoing to the position: x= " << goal.x << " y= " << goal.y << " theta = " << goal.theta << std::endl;
 		
-		if (action_client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
-			std::cout << "Target reached!" << std::endl;
+			while(!action_client.waitForResult(ros::Duration(3.0)) && start)
+				std::cout << "START IS " << start << std::endl;
+				
+			if(!start){
+				std::cout << "CANCELLING THE GOAL" << std::endl;
+				action_client.cancelGoal();
+			}
+			
+			if (action_client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
+				std::cout << "Target reached!" << std::endl;
+			}
 		}
-   	}
    }
+   
    return 0;
 }
