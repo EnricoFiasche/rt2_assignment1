fa8691c2fd5120687b46bde81d92441521f6abb5
EnricoFiasche <enrico.fiasche@gmail.com>
2021-05-12 10:15:26 +0000

    ros2 package almost finished

--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -1,47 +1,112 @@
-#include "ros/ros.h"
-#include "rt2_assignment1/Command.h"
-#include "rt2_assignment1/Position.h"
-#include "rt2_assignment1/RandomPosition.h"
+#include <memory>
+#include <inttypes.h>
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_components/register_node_macro.hpp"
 
-bool start = false;
+#include "rt2_assignment1/srv/command.hpp"
+#include "rt2_assignment1/srv/position.hpp"
+#include "rt2_assignment1/srv/random_position.hpp"
 
-bool user_interface(rt2_assignment1::Command::Request &req, rt2_assignment1::Command::Response &res){
-    if (req.command == "start"){
-    	start = true;
-    }
-    else {
-    	start = false;
-    }
-    return true;
+using Command = rt2_assignment1::srv::Command;
+using Position = rt2_assignment1::srv::Position;
+using RandomPosition = rt2_assignment1::srv::RandomPosition;
+using std::placeholders::_1;
+using std::placeholders::_2;
+using std::placeholders::_3;
+
+namespace rt2_assignment1{
+
+	class StateMachine : public rclcpp::Node{
+
+		public:
+			
+			StateMachine(const rclcpp::NodeOptions & options) : Node("state_machine", options){
+				// creating the service to manage the 
+				service_ = this->create_service<Command>("/user_interface",std::bind(&StateMachine::user_interface, this, _1, _2, _3));
+				
+				pos_client_ = this->create_client<Position>("/go_to_point");
+				while(!pos_client_->wait_for_service(std::chrono::seconds(3))){
+					if(!rclcpp::ok()){
+						RCLCPP_ERROR(this->get_logger(), "position client interrupted");
+						return ;
+					}
+					RCLCPP_INFO(this->get_logger(), "waiting for position service to appear...");
+				}
+				
+				random_client_ = this->create_client<RandomPosition>("/position_server");
+				while(!random_client_->wait_for_service(std::chrono::seconds(3))){
+					if(!rclcpp::ok()){
+						RCLCPP_ERROR(this->get_logger(), "random position client interrupted");
+						return ;
+					}
+					RCLCPP_INFO(this->get_logger(), "waiting for random service to appear...");
+				}
+			}
+			
+			void fsm(){
+				auto rp = std::make_shared<RandomPosition::Request>();
+				rp->x_max = 5.0;
+			   	rp->x_min = -5.0;
+			   	rp->y_max = 5.0;
+			   	rp->y_min = -5.0;
+			   	
+			   	pos = std::make_shared<Position::Request>();
+			   	
+			   	if (this->start){
+			   	
+			   		using ServiceResponseFuture = rclcpp::Client<RandomPosition>::SharedFuture;
+				   	auto response_received = [this] (ServiceResponseFuture future) {
+				   		pos->x = future.get()->x;
+				   		pos->y = future.get()->y;
+				   		pos->theta = future.get()->theta;
+
+						std::cout << "\nGoing to the position: x = " << pos->x << " y = " << pos->y << " theta = " << pos->theta << std::endl;
+					};
+			   			
+			   		auto future_request = random_client_->async_send_request(rp, response_received);
+	   				
+	   				//pos->x = x; pos->y = y; pos->theta = theta;
+			   		
+			   		using ServicePosFuture = rclcpp::Client<Position>::SharedFuture;
+			   		auto response_pos = [this] (ServicePosFuture futurepos){
+			   			if(futurepos.get()->ok){
+			   				std::cout << "Goal Reached!" << std::endl;
+			   				fsm();
+			   			}
+			   		};
+			   			
+	   				auto future_request_pos = pos_client_->async_send_request(pos,response_pos);
+			   	}
+			}
+		
+		private:
+			
+			bool user_interface(const std::shared_ptr<rmw_request_id_t> request_header,
+							 	const std::shared_ptr<Command::Request> req,
+							 	const std::shared_ptr<Command::Response> res){
+				
+				(void)request_header;
+				
+				if (req->command == "start"){
+					this->start = true;
+					this->fsm();
+				}
+				else 
+					this->start = false;
+				
+				
+				std::cout << "COMMAND RECEIVED " << start << std::endl;
+
+				return true;
+			}
+			
+			bool start = false;
+			rclcpp::Service<Command>::SharedPtr service_;
+			rclcpp::Client<Position>::SharedPtr pos_client_;
+			rclcpp::Client<RandomPosition>::SharedPtr random_client_;
+			//double x,y,theta;
+			std::shared_ptr<Position::Request> pos;
+	};
 }
 
-
-int main(int argc, char **argv)
-{
-   ros::init(argc, argv, "state_machine");
-   ros::NodeHandle n;
-   ros::ServiceServer service= n.advertiseService("/user_interface", user_interface);
-   ros::ServiceClient client_rp = n.serviceClient<rt2_assignment1::RandomPosition>("/position_server");
-   ros::ServiceClient client_p = n.serviceClient<rt2_assignment1::Position>("/go_to_point");
-   
-   rt2_assignment1::RandomPosition rp;
-   rp.request.x_max = 5.0;
-   rp.request.x_min = -5.0;
-   rp.request.y_max = 5.0;
-   rp.request.y_min = -5.0;
-   rt2_assignment1::Position p;
-   
-   while(ros::ok()){
-   	ros::spinOnce();
-   	if (start){
-   		client_rp.call(rp);
-   		p.request.x = rp.response.x;
-   		p.request.y = rp.response.y;
-   		p.request.theta = rp.response.theta;
-   		std::cout << "\nGoing to the position: x= " << p.request.x << " y= " <<p.request.y << " theta = " <<p.request.theta << std::endl;
-   		client_p.call(p);
-   		std::cout << "Position reached" << std::endl;
-   	}
-   }
-   return 0;
-}
+RCLCPP_COMPONENTS_REGISTER_NODE(rt2_assignment1::StateMachine)
