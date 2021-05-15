402d499612fefaa5b72ae10ce2b84b34d39d64fd
EnricoFiasche <enrico.fiasche@gmail.com>
2021-05-15 15:46:23 +0000

    Commented src/ files

--- src/random_point_server.cpp
+++ src/random_point_server.cpp
@@ -5,7 +5,7 @@
 
 #include "rt2_assignment1/srv/random_position.hpp"
 
-using RandomPosition = rt2_assignment1::srv::RandomPosition;
+using RandomPosition = rt2_assignment1::srv::RandomPosition; /** RandomPosition service */
 using std::placeholders::_1;
 using std::placeholders::_2;
 using std::placeholders::_3;
@@ -15,7 +15,9 @@ namespace rt2_assignment1{
 	class PositionService : public rclcpp::Node
 	{
 		public:
-		
+			/**
+			 *  Position service constructor that creates a new service
+			 */
 			PositionService(const rclcpp::NodeOptions & options) : Node("random_position_server", options){
 				// creating a new service for RandomPosition
 				service_ = this->create_service<RandomPosition>("/position_server",std::bind(&PositionService::myrandom, this, _1, _2, _3));
@@ -23,10 +25,32 @@ namespace rt2_assignment1{
 		
 		private:
 		
+			/**
+ 			  *  Function used to return a random number between the interval
+ 			  *  [N,M]
+			  *
+			  *  @param:
+			  *		M: minimum value allowed for the random number
+			  *		N: maximum value allowed for the random number 
+			  *
+			  *  @return: random value between M and N
+ 			  */
 			double randMToN(double M, double N)
 			{     return M + (rand() / ( RAND_MAX / (N-M) ) ) ; }
 
-
+			/**
+			 *  Function used by the server to generate the coordinate of a
+			 *  random goal, x position, y position and theta (the orientation).
+			 *
+			 *  @param:
+			 *  request_header: to avoid warnings.
+			 *	req: request done by a client, where is stored the min and
+			 *	     max value allowed for x and y position.
+			 *	res: response of the server, where is stored the three
+			 *	     random coordinates.
+			 *
+			 *  @return: true value when the function finish to store the response.
+			 */
 			bool myrandom (const std::shared_ptr<rmw_request_id_t> request_header,
 						   const std::shared_ptr<RandomPosition::Request> req,
 						   const std::shared_ptr<RandomPosition::Response> res){
@@ -37,7 +61,7 @@ namespace rt2_assignment1{
 				return true;
 			}
 			
-			// variable used to manage the server
+			/** variable used to manage the server */
 			rclcpp::Service<RandomPosition>::SharedPtr service_;
 	};
 }
--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -20,6 +20,11 @@ namespace rt2_assignment1{
 
 		public:
 			
+			/**
+			 *  StateMachine constructor that a server, which allows the node to read the command sent by the user,
+			 *  and two, one to get a random target point and one to communicate with go_to_point, which move
+			 *  the robot in order to reach the goal.
+			 */
 			StateMachine(const rclcpp::NodeOptions & options) : Node("state_machine", options){
 				// creating the service to manage the 
 				service_ = this->create_service<Command>("/user_interface",std::bind(&StateMachine::user_interface, this, _1, _2, _3));
@@ -43,6 +48,12 @@ namespace rt2_assignment1{
 				}
 			}
 			
+			/**
+			 *  Function used to make request to the RandomPosition server and to send the
+			 *  goal position to go_to_point.
+			 *  If the value of start is true, the function makes a request to the RandomPosition server
+			 *  in order to get the next goal, then sends it to go_to_point and waits the result. 
+			 */
 			void fsm(){
 				auto rp = std::make_shared<RandomPosition::Request>();
 				rp->x_max = 5.0;
@@ -50,35 +61,50 @@ namespace rt2_assignment1{
 			   	rp->y_max = 5.0;
 			   	rp->y_min = -5.0;
 			   	
-			   	pos = std::make_shared<Position::Request>();
+			   	goal_pos = std::make_shared<Position::Request>();
 			   	
 			   	if (this->start){
 			   	
 			   		using ServiceResponseFuture = rclcpp::Client<RandomPosition>::SharedFuture;
 				   	auto response_received = [this] (ServiceResponseFuture future) {
-				   		pos->x = future.get()->x;
-				   		pos->y = future.get()->y;
-				   		pos->theta = future.get()->theta;
+				   		// getting the goal position from the response
+				   		goal_pos->x = future.get()->x;
+				   		goal_pos->y = future.get()->y;
+				   		goal_pos->theta = future.get()->theta;
 
-						std::cout << "\nGoing to the position: x = " << pos->x << " y = " << pos->y << " theta = " << pos->theta << std::endl;
+						std::cout << "\nGoing to the position: x = " << goal_pos->x << " y = " << goal_pos->y << " theta = " << goal_pos->theta << std::endl;
 						
 						using ServicePosFuture = rclcpp::Client<Position>::SharedFuture;
 				   		auto response_pos = [this] (ServicePosFuture futurepos){
+				   			// if the target is reached print a success message
 				   			if(futurepos.get()->ok){
 				   				std::cout << "Goal Reached!" << std::endl;
 				   				fsm();
 				   			}
 				   		};
-				   		
-				   		auto future_request_pos = goto_client_->async_send_request(pos,response_pos);
+				   		// send an async request
+				   		auto future_request_pos = goto_client_->async_send_request(goal_pos,response_pos);
 					};
-			   			
+			   		// send an async request
 			   		auto future_request = random_client_->async_send_request(rp, response_received);
 			   	}
 			}
 		
 		private:
 			
+			/**
+			 *  Function of the user_interface server used to modify the value of
+			 *  the variable "start", based on the command received by the client. 
+			 *
+			 *  @param:
+			 *	request_header: to avoid warnings.
+			 *	req: request sent by the client, which could be "start"
+			 *	     or "stop".
+			 *	res: response of the server, which is "true" when it finishes
+			 *	    to modify the start value.
+			 *
+			 *  @return true when it finishes to modify the start value
+			 */
 			bool user_interface(const std::shared_ptr<rmw_request_id_t> request_header,
 							 	const std::shared_ptr<Command::Request> req,
 							 	const std::shared_ptr<Command::Response> res){
@@ -87,22 +113,19 @@ namespace rt2_assignment1{
 				
 				if (req->command == "start"){
 					this->start = true;
-					this->fsm();
+					this->fsm(); // call the function finite state machine
 				}
 				else 
 					this->start = false;
-				
-				
-				std::cout << "COMMAND RECEIVED " << start << std::endl;
 
 				return true;
 			}
 			
-			bool start = false;
-			rclcpp::Service<Command>::SharedPtr service_;
-			rclcpp::Client<Position>::SharedPtr goto_client_;
-			rclcpp::Client<RandomPosition>::SharedPtr random_client_;
-			std::shared_ptr<Position::Request> pos;
+			bool start = false; /** bool variable that stores the command sent by the user */
+			rclcpp::Service<Command>::SharedPtr service_; /** service used to manage user_interface */
+			rclcpp::Client<Position>::SharedPtr goto_client_; /** go_to_point client */
+			rclcpp::Client<RandomPosition>::SharedPtr random_client_; /** random position client */
+			std::shared_ptr<Position::Request> goal_pos; /** goal position */
 	};
 }
 
